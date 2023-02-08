c4f991a3e3ae47c14daefe652b1884b11b8bbd65
Almir CB <acb310892@gmail.com>
2023-02-08 17:20:52 -0300

    Started doing some styling stuff but have no time, doing a commit before leaving pc

--- frontend/src/components/Page.js
+++ frontend/src/components/Page.js
@@ -7,8 +7,10 @@ import { makeStyles } from '@mui/material/styles';
 import Paper from '@mui/material/Paper'
 import Grid from '@mui/material/Grid'
 import { Typography } from '@mui/material';
+import { withStyles } from '@mui/material';
+import { createTheme } from '@mui/system';
 
-
+const theme = createTheme();
 // const useStyles = makeStyles((theme) => ({
 //     root: {
 //       flexGrow: 1,
@@ -30,14 +32,34 @@ import { Typography } from '@mui/material';
     
 //   }));
 
+const styles = {
+    root: {
+              flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    title: {
+        color: theme.palette.text.primary,
+    },
+    oneLine: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexFlow: "row nowrap",
+    }
+}
+
 function Header(){
     // const classes = useStyles();
     return(
-        <Paper className="paper">
+        <Paper className="paper" sx={styles.paper}>
             <Grid container spacing={3}>
                 <Grid xs={3}>
                     <div><img style={{height: "300px"}} src={ require('../static/images/photo_header.png') } /></div>
-                    <div className="oneLine"><CakeIcon /><Typography variant="caption" component="caption" >31/08/1992</Typography></div>
+                    <div className="oneLine" sx={styles.oneLine}><CakeIcon /><Typography variant="caption" component="caption" >31/08/1992</Typography></div>
                 </Grid>    
                 <Grid xs={6}>
                     <Typography variant="h2" component="h2" gutterBottom className="title">
@@ -54,9 +76,9 @@ function Header(){
                     alignItems="flex-center"
                     spacing={1}
                 >
-                <Grid xs={12} className="oneLine"><HomeIcon /><Typography variant="caption" component="caption">Barcelona</Typography></Grid>
-                <Grid xs={12} className="oneLine"><PhoneIcon /><Typography variant="caption" component="caption">695544183</Typography></Grid>
-                <Grid xs={12} className="oneLine"><MailIcon /><Typography variant="caption" component="caption">almir.cabe@gmail.com</Typography></Grid>
+                <Grid xs={12} className="oneLine" sx={styles.oneLine}><HomeIcon /><Typography variant="caption" component="caption">Barcelona</Typography></Grid>
+                <Grid xs={12} className="oneLine" sx={styles.oneLine}><PhoneIcon /><Typography variant="caption" component="caption">695544183</Typography></Grid>
+                <Grid xs={12} className="oneLine" sx={styles.oneLine}><MailIcon /><Typography variant="caption" component="caption">almir.cabe@gmail.com</Typography></Grid>
                 </Grid>
             </Grid>
         </Paper>
@@ -75,21 +97,21 @@ export default function PageGrid() {
             <Header></Header>
           </Grid>
           <Grid xs={9}>
-            <Paper className="paper">xs</Paper>
+            <Paper className="paper" sx={styles.paper}>xs</Paper>
           </Grid>
           <Grid xs={3}>
-            <Paper className="paper">xs</Paper>
+            <Paper className="paper" sx={styles.paper}>xs</Paper>
           </Grid>
         </Grid>
         <Grid container spacing={3}>
           <Grid xs>
-            <Paper className="paper">xs</Paper>
+            <Paper className="paper" sx={styles.paper}>xs</Paper>
           </Grid>
           <Grid xs={6}>
-            <Paper className="paper">xs=6</Paper>
+            <Paper className="paper" sx={styles.paper}>xs=6</Paper>
           </Grid>
           <Grid xs>
-            <Paper className="paper">xs</Paper>
+            <Paper className="paper" sx={styles.paper}>xs</Paper>
           </Grid>
         </Grid>
       </div>
